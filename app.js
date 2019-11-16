f07c399f7fffe702ac03fff14b39b2f66c6fd18d
Eddie DePaolo <edepaolo@gmail.com>
2019-11-15 22:10:44 -0800

    firebase link added to app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCTurKTyM9ZWfOyf8b9OEy6oPGkHgyHc3Y",
+    authDomain: "train-scheduler-ce305.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ce305.firebaseio.com",
+    projectId: "train-scheduler-ce305",
+    storageBucket: "train-scheduler-ce305.appspot.com",
+    messagingSenderId: "518272482182",
+    appId: "1:518272482182:web:7d25289d84d0568372ca07"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
